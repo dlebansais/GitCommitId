3e892968c39fa497060801db5e0819629622e620
David Le Bansais <dlebansais@gmail.com>
2018-10-31 13:36:21 +0100

    Added -w and the repo address.

--- GitCommitId/Program.cs
+++ GitCommitId/Program.cs
@@ -24,10 +24,12 @@ namespace GitCommitId
 
     public class Program
     {
+        private static readonly string RepositoryAddressString = "RepositoryAddress";
         private static readonly string CommitIdString = "CommitId";
         private static bool IsUpdate = false;
         private static bool IsReplace = false;
         private static bool IsClear = false;
+        private static bool UseWorkingDirectory = false;
         private static bool IsQuiet = false;
 
         private static int Main(string[] args)
@@ -36,11 +38,12 @@ namespace GitCommitId
             {
                 string[] CommandLineArgs = Environment.GetCommandLineArgs();
                 string ProgramName = CommandLineArgs.Length > 0 ? Path.GetFileNameWithoutExtension(CommandLineArgs[0]) : "program";
-                Console.WriteLine($"Usage: {ProgramName} <.exe or .dll> [-u | -r | -c] [-q]");
+                Console.WriteLine($"Usage: {ProgramName} <.exe or .dll> [-u | -r | -c] [-w] [-q]");
                 Console.WriteLine("No option: read the Commit Id in the file.");
                 Console.WriteLine("-u: update the file Commit Id with the current Id, except if no change.");
                 Console.WriteLine("-r: replace the file Commit Id with the current Id even if no change, and insert it if necessary.");
                 Console.WriteLine("-c: clear the Commit Id in the file.");
+                Console.WriteLine("-w: search Git in the working directory.");
                 Console.WriteLine("-q: quiet.");
                 return ToReturnCode(Errors.MissingArgument);
             }
@@ -55,6 +58,8 @@ namespace GitCommitId
                     IsReplace = true;
                 else if (Option == "-c")
                     IsClear = true;
+                else if (Option == "-w")
+                    UseWorkingDirectory = true;
                 else if (Option == "-q")
                     IsQuiet = true;
                 else
@@ -73,14 +78,14 @@ namespace GitCommitId
             }
 
             if (IsUpdate || IsReplace)
-                return UpdateCommitId(FileName, IsReplace);
+                return UpdateFileCommitId(FileName, IsReplace);
             else if (IsClear)
-                return ClearCommitId(FileName);
+                return ClearFileCommitId(FileName);
             else
-                return ReadCommitId(FileName);
+                return ReadFileCommitId(FileName);
         }
 
-        private static int ReadCommitId(string fileName)
+        private static int ReadFileCommitId(string fileName)
         {
             try
             {
@@ -90,14 +95,16 @@ namespace GitCommitId
 
                 try
                 {
-                    string CommitId = StringFileInfo[CommitIdString];
+                    string RepositoryAddress = ReadResourceString(StringFileInfo, RepositoryAddressString);
+                    string CommitId = ReadResourceString(StringFileInfo, CommitIdString);
 
+                    Output($"Current Repo: {RepositoryAddress}");
                     Output($"Current Id: {CommitId}");
                     return ToReturnCode(Errors.Success);
                 }
                 catch
                 {
-                    Output("File doesn't contain a Commit Id.");
+                    Output("File doesn't contain Git info.");
                     return ToReturnCode(Errors.NoCommitId);
                 }
             }
@@ -108,11 +115,11 @@ namespace GitCommitId
             }
         }
 
-        private static int UpdateCommitId(string fileName, bool forceReplace)
+        private static int UpdateFileCommitId(string fileName, bool forceReplace)
         {
             int Result;
-
-            if ((Result = GetCommitId(fileName, out string CommitId)) < 0)
+           
+            if ((Result = GetCommitInfo(fileName, out string RepositoryAddress, out string CommitId)) < 0)
                 return Result;
 
             try
@@ -121,23 +128,42 @@ namespace GitCommitId
                 VersionResource.LoadFrom(fileName);
                 StringFileInfo StringFileInfo = (StringFileInfo)VersionResource["StringFileInfo"];
 
+                string FileRepositoryAddress;
                 string FileCommitId;
                 try
                 {
-                    FileCommitId = StringFileInfo[CommitIdString];
+                    FileRepositoryAddress = ReadResourceString(StringFileInfo, RepositoryAddressString);
+                    FileCommitId = ReadResourceString(StringFileInfo, CommitIdString);
                 }
                 catch
                 {
+                    FileRepositoryAddress = null;
                     FileCommitId = null;
                 }
 
+                bool IsRepositoryAddressUpdated;
+                if (forceReplace || FileRepositoryAddress == null || FileRepositoryAddress != RepositoryAddress)
+                {
+                    WriteResourceString(StringFileInfo, RepositoryAddressString, RepositoryAddress);
+                    Output($"Repo updated to: {RepositoryAddress}");
+                    IsRepositoryAddressUpdated = true;
+                }
+                else
+                    IsRepositoryAddressUpdated = false;
+
+                bool IsCommitIdUpdated;
                 if (forceReplace || FileCommitId == null || FileCommitId != CommitId)
                 {
-                    StringFileInfo[CommitIdString] = CommitId;
-                    VersionResource.SaveTo(fileName);
-
+                    WriteResourceString(StringFileInfo, CommitIdString, CommitId);
                     Output($"Id updated to: {CommitId}");
+
+                    IsCommitIdUpdated = true;
                 }
+                else
+                    IsCommitIdUpdated = false;
+
+                if (IsRepositoryAddressUpdated || IsCommitIdUpdated)
+                    VersionResource.SaveTo(fileName);
 
                 return ToReturnCode(Errors.Success);
             }
@@ -148,12 +174,78 @@ namespace GitCommitId
             }
         }
 
-        private static int GetCommitId(string fileName, out string commitId)
+        private static int GetCommitInfo(string fileName, out string repositoryAddress, out string commitId)
         {
-            string Folder = Path.GetDirectoryName(fileName);
-            while (Folder != null)
+            string Folder = UseWorkingDirectory ? Environment.CurrentDirectory : Path.GetDirectoryName(fileName);
+
+            int Result;
+
+            if ((Result = GetRepositoryAddress(Folder, out repositoryAddress)) < 0)
             {
-                string GitPath = Path.Combine(Folder, ".git/HEAD");
+                commitId = null;
+                return Result;
+            }
+
+            if ((Result = GetCommitId(Folder, out commitId)) < 0)
+                return Result;
+
+            return ToReturnCode(Errors.Success);
+        }
+
+        private static int GetRepositoryAddress(string folder, out string repositoryAddress)
+        {
+            while (folder != null)
+            {
+                string GitPath = Path.Combine(folder, ".git/config");
+                if (File.Exists(GitPath))
+                {
+                    try
+                    {
+                        using (FileStream fs = new FileStream(GitPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+                        {
+                            using (StreamReader sr = new StreamReader(fs, Encoding.ASCII))
+                            {
+                                for (; ; )
+                                {
+                                    string Line = sr.ReadLine();
+                                    if (Line == null)
+                                        break;
+
+                                    Line = Line.Trim();
+                                    if (Line.StartsWith("url = "))
+                                    {
+                                        repositoryAddress = Line.Substring(6);
+                                        return ToReturnCode(Errors.Success);
+                                    }
+                                }
+                            }
+                        }
+
+                        Output("Invalid Git repository.");
+                        repositoryAddress = null;
+                        return ToReturnCode(Errors.InvalidGitRepository);
+                    }
+                    catch (Exception e)
+                    {
+                        Output(e.Message);
+                        repositoryAddress = null;
+                        return ToReturnCode(Errors.ExceptionReadingGit);
+                    }
+                }
+                else
+                    folder = Path.GetDirectoryName(folder);
+            }
+
+            Output("File not in a subdirectory of a Git repository.");
+            repositoryAddress = null;
+            return ToReturnCode(Errors.NotInGitRepository);
+        }
+
+        private static int GetCommitId(string folder, out string commitId)
+        {
+            while (folder != null)
+            {
+                string GitPath = Path.Combine(folder, ".git/HEAD");
                 if (File.Exists(GitPath))
                 {
                     try
@@ -171,7 +263,7 @@ namespace GitCommitId
 
                         if (Head != null)
                         {
-                            GitPath = Path.Combine(Folder, $".git/{Head}");
+                            GitPath = Path.Combine(folder, $".git/{Head}");
                             if (File.Exists(GitPath))
                             {
                                 using (FileStream fs = new FileStream(GitPath, FileMode.Open, FileAccess.Read, FileShare.Read))
@@ -197,7 +289,7 @@ namespace GitCommitId
                     }
                 }
                 else
-                    Folder = Path.GetDirectoryName(Folder);
+                    folder = Path.GetDirectoryName(folder);
             }
 
             Output("File not in a subdirectory of a Git repository.");
@@ -205,7 +297,7 @@ namespace GitCommitId
             return ToReturnCode(Errors.NotInGitRepository);
         }
 
-        private static int ClearCommitId(string fileName)
+        private static int ClearFileCommitId(string fileName)
         {
             try
             {
@@ -213,18 +305,35 @@ namespace GitCommitId
                 VersionResource.LoadFrom(fileName);
                 StringFileInfo StringFileInfo = (StringFileInfo)VersionResource["StringFileInfo"];
 
+                bool IsRepositoryAddressRemoved = false;
+                foreach (KeyValuePair<string, StringTable> StringEntry in StringFileInfo.Strings)
+                    foreach (KeyValuePair<string, StringTableEntry> Entry in StringEntry.Value.Strings)
+                        if (Entry.Key == RepositoryAddressString)
+                        {
+                            StringEntry.Value.Strings.Remove(RepositoryAddressString);
+                            Output("Repo removed.");
+                            IsRepositoryAddressRemoved = true;
+                            break;
+                        }
+
+                bool IsCommitIdRemoved = false;
                 foreach (KeyValuePair<string, StringTable> StringEntry in StringFileInfo.Strings)
                     foreach (KeyValuePair<string, StringTableEntry> Entry in StringEntry.Value.Strings)
                         if (Entry.Key == CommitIdString)
                         {
                             StringEntry.Value.Strings.Remove(CommitIdString);
-                            VersionResource.SaveTo(fileName);
                             Output("Commit Id removed.");
-
-                            return ToReturnCode(Errors.Success);
+                            IsCommitIdRemoved = true;
+                            break;
                         }
 
-                Output("No Commit Id to remove.");
+                if (IsRepositoryAddressRemoved || IsCommitIdRemoved)
+                {
+                    VersionResource.SaveTo(fileName);
+                    return ToReturnCode(Errors.Success);
+                }
+
+                Output("Nothing to remove.");
                 return ToReturnCode(Errors.Success);
             }
             catch (Exception e)
@@ -232,6 +341,21 @@ namespace GitCommitId
                 Output(e.Message);
                 return ToReturnCode(Errors.ExceptionClearingFile);
             }
+        }
+
+        private static string ReadResourceString(StringFileInfo stringFileInfo, string key)
+        {
+            string Result = stringFileInfo[key];
+
+            if (Result != null && Result.Length > 0 && Result[Result.Length - 1] == '\0')
+                Result = Result.Substring(0, Result.Length - 1);
+
+            return Result;
+        }
+
+        private static void WriteResourceString(StringFileInfo stringFileInfo, string key, string value)
+        {
+            stringFileInfo[key] = value;
         }
 
         private static void Output(string message)
--- README.md
+++ README.md
@@ -13,11 +13,34 @@ An assembly compiled with source code from a Git repository can be tagged with t
     -u: update the file Commit Id with the current Id, except if no change.
     -r: replace the file Commit Id with the current Id even if no change, and insert it if necessary.
     -c: clear the Commit Id in the file.
+    -w: search Git in the working directory.
     -q: quiet.
 
 This program can be used in Visual Studio Solutions, in post-build events, or as a standalone program. If you sign the binary with for example [signtool.exe](https://docs.microsoft.com/en-us/dotnet/framework/tools/signtool-exe), make sure you update the Commit Id with `GitCommitId.exe $(TargetPath) -u` before you sign it, or this will destroy the digital signature. 
   
 ## How does it work?
 
-The program adds a "CommitId" string pair in the FILEVERSION resource of the file.
- 
+The program adds a "RepositoryAddress" and "CommitId" string pairs in the FILEVERSION resource of the file with the corresponding information: the address of the Git repository (such as https://github.com/myname/myrepo) and the Commit Id of the head in the local copy.
+
+Git information come from searching for Git files, starting where the target assembly is located, unless the -w option is used.
+
+## Example
+
+    GitCommitId myfile.exe
+    File doesn't contain Git info.
+
+    GitCommitId myfile.exe -u
+    Repo updated to: https://github.com/myname/myrepo
+    Id updated to: 71fb4cd62172257132259d2d00ed9c57e253c72d
+
+    GitCommitId myfile.exe
+    Current Repo: https://github.com/myname/myrepo
+    Current Id: 71fb4cd62172257132259d2d00ed9c57e253c72d
+
+    GitCommitId myfile.exe -c
+    Repo removed.
+    Commit Id removed.
+
+    GitCommitId myfile.exe
+    File doesn't contain Git info.
+  
